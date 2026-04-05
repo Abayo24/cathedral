8514ec3ef1debcb034db02cfad6ce98e88985e52
Abayo <eabayo@konza.ke>
2026-04-05 18:07:54 +0300

    Fix live.ts import error after downgrading next-sanity

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,9 +1,12 @@
-// Querying with "sanityFetch" will keep content automatically updated
-// Before using it, import and render "<SanityLive />" in your layout, see
-// https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity/live";
-import { client } from './client'
+// sanity/lib/live.ts
+import { client } from './client';
 
-export const { sanityFetch, SanityLive } = defineLive({
-  client,
-});
+// We downgraded next-sanity, so we use standard fetch instead of the Live API
+export const sanityFetch = async ({ query, params = {} }: any) => {
+  return client.fetch(query, params);
+};
+
+// Return an empty component so if <SanityLive /> is imported anywhere, it won't crash
+export function SanityLive() {
+  return null;
+}
